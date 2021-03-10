67383a0d20243fef4600af418d0278027ac0779c
naTpuk-ep <naTpuk.ep@gmail.com>
2021-03-10 16:23:54 +0300

    init: deploy

--- db/pg.ts
+++ db/pg.ts
@@ -11,7 +11,7 @@ const {
 } = process.env;
 
 const url = DATABASE_URL
-	? `${DATABASE_URL}?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory`
+	? `${DATABASE_URL}`
 	: `postgres://${PG_USERNAME}:${PG_PASSWORD}@${PG_HOST}/${PG_DBNAME}`;
 
 console.log("URL", url);
